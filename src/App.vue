2fe929cdad4d02af7e66a25409ae713e65847b65
meNmae <me>
2025-10-18 19:43:16 +0800

    resizebar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
-import CodeView from './components/CodeView.vue';
+import CodeView from './components/CodeView.vue'
+import ResizeBar from './components/ResizeBar.vue'
 </script>
 <style scoped>
     .head{
@@ -9,19 +10,28 @@ import CodeView from './components/CodeView.vue';
         height: 100px;
         background-color: rgb(136, 136, 136);
     }
+    #page-root{
+        position: relative;
+        width: 100%;
+        height: 100%;
+        
+
+    }
+    .layer{
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
 </style>
 <template>
-    <div style="width: 100%;">
-        <header class="head">
+    <div id="page-root">
+        <div id="resize-bars" class="layer">
 
-
-        </header>
-
-        <main>
-        
-        </main>
+        </div>
+        <div id="pages" class="layer">
+            
+        </div>
     </div>
-    
 </template>
 
 
---
+++ src/components/ResizeBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="js">
+</script>
+<style scoped>
+    .resize-bar{
+        z-index: 100;
+    }
+    .resize-bar:hover{
+        border: 5px solid red;
+    }
+</style>
+<template>
+    <div class="resize-bar">
+
+    </div>
+</template>
